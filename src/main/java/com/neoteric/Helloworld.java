7e231a84b22fae278eed0d8ff688b69b8b372364
manoj89482 <manojchowdary894@gmail.com>
2025-02-24 16:54:23 +0530

    hello

--- src/main/java/com/neoteric/Helloworld.java
+++ src/main/java/com/neoteric/Helloworld.java
@@ -2,6 +2,6 @@ package com.neoteric;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world say");
     }
 }
